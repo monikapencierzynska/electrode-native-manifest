d08d42dbfaa4e9b843a07e95e959921487758cd9
FlorBosch <florbosch128@gmail.com>
2021-11-23 15:07:22 +0100

    Update plugins

--- plugins/ern_v0.14.0+/@react-native-community/masked-view_v0.1.10+/RNCMaskedViewPackagePlugin.java
+++ plugins/ern_v0.14.0+/@react-native-community/masked-view_v0.1.10+/RNCMaskedViewPackagePlugin.java
@@ -4,11 +4,13 @@ import android.app.Application;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
+import com.facebook.react.ReactInstanceManagerBuilder;
 import com.facebook.react.ReactPackage;
 import org.reactnative.maskedview.RNCMaskedViewPackage;
 
 public class RNCMaskedViewPackagePlugin implements ReactPlugin {
-    public ReactPackage hook(@NonNull Application application, @Nullable ReactPluginConfig config) {
+    public ReactPackage hook(@NonNull Application application,
+                     @Nullable ReactPluginConfig config) {
         return new RNCMaskedViewPackage();
     }
 }
